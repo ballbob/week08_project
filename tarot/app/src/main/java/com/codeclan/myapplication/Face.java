d1c560ba616d0f696094e3d09c7b6932eae37e8e
Robert Ball <robert.ball.1989@gmail.com>
2017-03-19 22:07:51 +0000

    Created face and suit enums. Bouts will be hard-coded with their values.

---
+++ tarot/app/src/main/java/com/codeclan/myapplication/Face.java
@@ -0,0 +1,23 @@
+package com.codeclan.myapplication;
+
+/**
+ * Created by user on 19/03/2017.
+ */
+
+public enum Face {
+    BOUT,
+    KING,
+    QUEEN,
+    KNIGHT,
+    JACK,
+    TEN,
+    NINE,
+    EIGHT,
+    SEVEN,
+    SIX,
+    FIVE,
+    FOUR,
+    THREE,
+    TWO,
+    ACE
+}
---
+++ tarot/app/src/main/java/com/codeclan/myapplication/Suit.java
@@ -0,0 +1,13 @@
+package com.codeclan.myapplication;
+
+/**
+ * Created by user on 19/03/2017.
+ */
+
+public enum Suit {
+    BOUT,
+    HEARTS,
+    SPADES,
+    CLUBS,
+    DIAMONDS
+}
